26080cf073b1d45c9e1eb2c4737e96110e33137a
Zahra Rostami <zahrarostami137878@gmail.com>
2025-02-28 20:44:57 +0330

    edit App: add API and use useFetchTasksData hook for fetch data and import Table component

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,9 +1,20 @@
-function App() {
+import React, { useEffect, useState } from 'react';
+import useFetchTasksData from './hooks/useFetchTasksData';
+import Table from './components/table/Table';
 
+function App() {
+  const tasksDataAPI = "http://localhost:3000/TasksData";
+  const tasksTableData = useFetchTasksData(tasksDataAPI);
+  const taskTableHeaders = ["Id", "Title", "Date"];
 
   return (
     <>
-     <div>Hello</div>
+      <div className='flex justify-center items-center min-h-screen'>
+        <Table 
+          data={tasksTableData}
+          headers={taskTableHeaders}
+           />
+      </div>
     </>
   )
 }
